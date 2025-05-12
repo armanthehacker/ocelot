1361f8cb042f547ad1a429ac6064cbc3111323ef
actuallylemoncurd <curtis.x.jenkins@gmail.com>
2025-05-12 14:07:39 -0400

    vwpla

---
+++ firmware/vw_pla/main.c
@@ -0,0 +1,402 @@
+/*
+CUSTOM gateway implementation for the RSG OFFROAD Tundra-swapped 4-runner.
+This board will convert the Toyota PTCAN signals to RetroPilot-compatible signals.
+Additionally, this board handles the cruise control switch functions. Hopefully, 
+it will also be able to switch "modes" by intercepting the stalk.
+*/
+
+// ********************* Includes *********************
+#include "../config.h"
+#include "libc.h"
+
+#include "main_declarations.h"
+#include "critical.h"
+#include "faults.h"
+
+#include "drivers/registers.h"
+#include "drivers/interrupts.h"
+#include "drivers/llcan.h"
+#include "drivers/llgpio.h"
+#include "drivers/adc.h"
+
+
+#include "board.h"
+
+#include "drivers/clock.h"
+#include "drivers/dac.h"
+#include "drivers/timer.h"
+
+#include "gpio.h"
+#include "crc.h"
+
+#include "drivers/uart.h"
+#include "drivers/usb.h"
+
+#define ENTER_BOOTLOADER_MAGIC 0xdeadbeef
+uint32_t enter_bootloader_mode;
+
+// cppcheck-suppress unusedFunction ; used in headers not included in cppcheck
+void __initialize_hardware_early(void) {
+  early();
+}
+
+#include "vw_pla/can.h"
+
+// ********************* usb debugging *********************
+void debug_ring_callback(uart_ring *ring) {
+  char rcv;
+  while (getc(ring, &rcv) != 0) {
+    (void)putc(ring, rcv);
+  }
+}
+
+int usb_cb_ep1_in(void *usbdata, int len, bool hardwired) {
+  UNUSED(hardwired);
+  CAN_FIFOMailBox_TypeDef *reply = (CAN_FIFOMailBox_TypeDef *)usbdata;
+  int ilen = 0;
+  while (ilen < MIN(len/0x10, 4) && can_pop(&can_rx_q, &reply[ilen])) {
+    ilen++;
+  }
+  return ilen*0x10;
+}
+// send on serial, first byte to select the ring
+void usb_cb_ep2_out(void *usbdata, int len, bool hardwired) {
+  UNUSED(hardwired);
+  uint8_t *usbdata8 = (uint8_t *)usbdata;
+  uart_ring *ur = get_ring_by_number(usbdata8[0]);
+  if ((len != 0) && (ur != NULL)) {
+    if ((usbdata8[0] < 2U)) {
+      for (int i = 1; i < len; i++) {
+        while (!putc(ur, usbdata8[i])) {
+          // wait
+        }
+      }
+    }
+  }
+}
+// send on CAN
+void usb_cb_ep3_out(void *usbdata, int len, bool hardwired) {
+  UNUSED(usbdata);
+  UNUSED(len);
+  UNUSED(hardwired);
+}
+void usb_cb_ep3_out_complete() {
+  if (can_tx_check_min_slots_free(MAX_CAN_MSGS_PER_BULK_TRANSFER)) {
+    usb_outep3_resume_if_paused();
+  }
+}
+
+void usb_cb_enumeration_complete() {
+  puts("USB enumeration complete\n");
+  is_enumerated = 1;
+}
+
+int usb_cb_control_msg(USB_Setup_TypeDef *setup, uint8_t *resp, bool hardwired) {
+  UNUSED(hardwired);
+  unsigned int resp_len = 0;
+  uart_ring *ur = NULL;
+  switch (setup->b.bRequest) {
+    // **** 0xd1: enter bootloader mode
+    case 0xd1:
+      // this allows reflashing of the bootstub
+      // so it's blocked over wifi
+      switch (setup->b.wValue.w) {
+        case 0:
+          // only allow bootloader entry on debug builds
+          #ifdef ALLOW_DEBUG
+            if (hardwired) {
+              puts("-> entering bootloader\n");
+              enter_bootloader_mode = ENTER_BOOTLOADER_MAGIC;
+              NVIC_SystemReset();
+            }
+          #endif
+          break;
+        case 1:
+          puts("-> entering softloader\n");
+          enter_bootloader_mode = ENTER_SOFTLOADER_MAGIC;
+          NVIC_SystemReset();
+          break;
+        default:
+          puts("Bootloader mode invalid\n");
+          break;
+      }
+      break;
+    // **** 0xd8: reset ST
+    case 0xd8:
+      NVIC_SystemReset();
+      break;
+    // **** 0xe0: uart read
+    case 0xe0:
+      ur = get_ring_by_number(setup->b.wValue.w);
+      if (!ur) {
+        break;
+      }
+      // read
+      while ((resp_len < MIN(setup->b.wLength.w, MAX_RESP_LEN)) &&
+                         getc(ur, (char*)&resp[resp_len])) {
+        ++resp_len;
+      }
+      break;
+    // **** 0xf1: Clear CAN ring buffer.
+    case 0xf1:
+      if (setup->b.wValue.w == 0xFFFFU) {
+        puts("Clearing CAN Rx queue\n");
+        can_clear(&can_rx_q);
+      } else if (setup->b.wValue.w < BUS_MAX) {
+        puts("Clearing CAN Tx queue\n");
+        can_clear(can_queues[setup->b.wValue.w]);
+      } else {
+        puts("Clearing CAN CAN ring buffer failed: wrong bus number\n");
+      }
+      break;
+    // **** 0xf2: Clear UART ring buffer.
+    case 0xf2:
+      {
+        uart_ring * rb = get_ring_by_number(setup->b.wValue.w);
+        if (rb != NULL) {
+          puts("Clearing UART queue.\n");
+          clear_uart_buff(rb);
+        }
+        break;
+      }
+    default:
+      puts("NO HANDLER ");
+      puth(setup->b.bRequest);
+      puts("\n");
+      break;
+  }
+  return resp_len;
+}
+
+// ***************************** can port *****************************
+#define CAN_UPDATE  0x23F //bootloader
+#define COUNTER_CYCLE 0xFU
+#define LKA_COUNTER_CYCLE = 0x3FU
+
+void CAN1_TX_IRQ_Handler(void) {
+  process_can(0);
+}
+
+void CAN2_TX_IRQ_Handler(void) {
+  process_can(1);
+}
+
+void CAN3_TX_IRQ_Handler(void) {
+  process_can(2);
+}
+
+bool sent;
+
+#define MAX_TIMEOUT 50U
+uint32_t timeout = 0;
+
+// fault states
+uint8_t state = 0;
+#define NO_FAULT 0U
+#define FAULT_BAD_CHECKSUM 1U
+#define FAULT_SEND 2U
+#define FAULT_SCE 3U
+#define FAULT_STARTUP 4U
+#define FAULT_TIMEOUT 5U
+#define FAULT_INVALID 6U
+#define FAULT_COUNTER 7U
+
+void CAN1_RX0_IRQ_Handler(void) {
+  // PTCAN connects here
+  while ((CAN1->RF0R & CAN_RF0R_FMP0) != 0) {
+    uint16_t address = CAN1->sFIFOMailBox[0].RIR >> 21;
+    #ifdef DEBUG_CAN
+    puts("CAN2 RX: ");
+    puth(address);
+    puts("\n");
+    #endif
+    switch (address) {  
+      default: ;
+    }
+    can_rx(0);
+  }
+}
+
+void CAN1_SCE_IRQ_Handler(void) {
+  state = FAULT_SCE;
+  can_sce(CAN1);
+  llcan_clear_send(CAN1);
+}
+
+void CAN2_RX0_IRQ_Handler(void) {
+  while ((CAN2->RF0R & CAN_RF0R_FMP0) != 0) {
+    uint16_t address = CAN2->sFIFOMailBox[0].RIR >> 21;
+    #ifdef DEBUG_CAN
+    puts("CAN2 RX: ");
+    puth(address);
+    puts("\n");
+    #endif
+    switch (address) {
+      default: ;
+    }
+    can_rx(1);
+  }
+}
+
+void CAN2_SCE_IRQ_Handler(void) {
+  state = FAULT_SCE;
+  can_sce(CAN2);
+  llcan_clear_send(CAN2);
+}
+
+void CAN3_RX0_IRQ_Handler(void) {
+  while ((CAN3->RF0R & CAN_RF0R_FMP0) != 0) {
+    #ifdef DEBUG_CAN
+    uint16_t address = CAN3->sFIFOMailBox[0].RIR >> 21;
+    puts("CAN3 RX: ");
+    puth(address);
+    puts("\n");
+    #endif
+    can_rx(2);
+  }
+}
+
+void CAN3_SCE_IRQ_Handler(void) {
+  state = FAULT_SCE;
+  can_sce(CAN3);
+  llcan_clear_send(CAN3);
+}
+
+int to_signed(int d, int bits) {
+  int d_signed = d;
+  if (d >= (1 << MAX((bits - 1), 0))) {
+    d_signed = d - (1 << MAX(bits, 0));
+  }
+  return d_signed;
+}
+
+uint8_t can1_count_out = 0;
+void TIM3_IRQ_Handler(void) {
+  // cmain loop for sending 100hz messages
+  TIM3->SR = 0;
+}
+
+// ***************************** main code *****************************
+
+#define MAX_FADE 8192U
+void set_led(uint8_t color, bool enabled) {
+  switch (color){
+    case LED_RED:
+      set_gpio_output(GPIOC, 9, !enabled);
+      break;
+     case LED_GREEN:
+      set_gpio_output(GPIOC, 7, !enabled);
+      break;
+    case LED_BLUE:
+      set_gpio_output(GPIOC, 6, !enabled);
+      break;
+    default:
+      break;
+  }
+}
+
+void loop(void) {
+  watchdog_feed();
+}
+
+int main(void) {
+  // Init LED GPIOs
+  set_gpio_mode(GPIOC, 9, MODE_OUTPUT);
+  set_gpio_mode(GPIOC, 7, MODE_OUTPUT);
+  set_gpio_mode(GPIOC, 6, MODE_OUTPUT);
+
+  // Init interrupt table
+  init_interrupts(true);
+
+  REGISTER_INTERRUPT(CAN1_TX_IRQn, CAN1_TX_IRQ_Handler, CAN_INTERRUPT_RATE, FAULT_INTERRUPT_RATE_CAN_1)
+  REGISTER_INTERRUPT(CAN1_RX0_IRQn, CAN1_RX0_IRQ_Handler, CAN_INTERRUPT_RATE, FAULT_INTERRUPT_RATE_CAN_1)
+  REGISTER_INTERRUPT(CAN1_SCE_IRQn, CAN1_SCE_IRQ_Handler, CAN_INTERRUPT_RATE, FAULT_INTERRUPT_RATE_CAN_1)
+  REGISTER_INTERRUPT(CAN2_TX_IRQn, CAN2_TX_IRQ_Handler, CAN_INTERRUPT_RATE, FAULT_INTERRUPT_RATE_CAN_2)
+  REGISTER_INTERRUPT(CAN2_RX0_IRQn, CAN2_RX0_IRQ_Handler, CAN_INTERRUPT_RATE, FAULT_INTERRUPT_RATE_CAN_2)
+  REGISTER_INTERRUPT(CAN2_SCE_IRQn, CAN2_SCE_IRQ_Handler, CAN_INTERRUPT_RATE, FAULT_INTERRUPT_RATE_CAN_2)
+  REGISTER_INTERRUPT(CAN3_TX_IRQn, CAN3_TX_IRQ_Handler, CAN_INTERRUPT_RATE, FAULT_INTERRUPT_RATE_CAN_3)
+  REGISTER_INTERRUPT(CAN3_RX0_IRQn, CAN3_RX0_IRQ_Handler, CAN_INTERRUPT_RATE, FAULT_INTERRUPT_RATE_CAN_3)
+  REGISTER_INTERRUPT(CAN3_SCE_IRQn, CAN3_SCE_IRQ_Handler, CAN_INTERRUPT_RATE, FAULT_INTERRUPT_RATE_CAN_3)
+
+  // Should run at around 732Hz (see init below)
+  REGISTER_INTERRUPT(TIM3_IRQn, TIM3_IRQ_Handler, 1000U, FAULT_INTERRUPT_RATE_TIM3)
+
+  disable_interrupts();
+
+  // init devices
+  clock_init();
+  peripherals_init();
+  detect_configuration();
+  detect_board_type();
+  // init board
+  current_board->init();
+  // enable USB
+  USBx->GOTGCTL |= USB_OTG_GOTGCTL_BVALOVAL;
+  USBx->GOTGCTL |= USB_OTG_GOTGCTL_BVALOEN;
+  usb_init();
+
+  // init can
+  bool llcan_speed_set = llcan_set_speed(CAN1, 5000, false, false);
+  if (!llcan_speed_set) {
+    puts("Failed to set llcan1 speed");
+  }
+  llcan_speed_set = llcan_set_speed(CAN2, 5000, false, false);
+  if (!llcan_speed_set) {
+    puts("Failed to set llcan2 speed");
+  }
+  llcan_speed_set = llcan_set_speed(CAN3, 5000, false, false);
+  if (!llcan_speed_set) {
+    puts("Failed to set llcan3 speed");
+  }
+
+  bool ret = llcan_init(CAN1);
+  UNUSED(ret);
+  ret = llcan_init(CAN2);
+  UNUSED(ret);
+  ret = llcan_init(CAN3);
+  UNUSED(ret);
+
+  gen_crc_lookup_table(crc_poly, crc8_lut_1d);
+
+  // 48mhz / 65536 ~= 732
+  timer_init(TIM3, 7);
+  NVIC_EnableIRQ(TIM3_IRQn);
+
+  dac_init();
+  adc_init();
+
+  // turn on relay
+  set_gpio_mode(GPIOB, 0, MODE_OUTPUT);
+  set_gpio_output_type(GPIOB, 0, OUTPUT_TYPE_PUSH_PULL);
+  set_gpio_output(GPIOB, 0, 1);
+
+  watchdog_init();
+
+  puts("**** INTERRUPTS ON ****\n");
+  enable_interrupts();
+
+  uint64_t cnt = 0;
+  for (cnt=0;;cnt++) {
+    // useful for debugging, fade breaks = panda is overloaded
+    for(uint32_t fade = 0U; fade < MAX_FADE; fade += 1U){
+      set_led(LED_BLUE, true);
+      delay(fade >> 4);
+      set_led(LED_BLUE, false);
+      delay((MAX_FADE - fade) >> 4);
+    }
+
+    for(uint32_t fade = MAX_FADE; fade > 0U; fade -= 1U){
+      set_led(LED_GREEN, true);
+      delay(fade >> 4);
+      set_led(LED_GREEN, false);
+      delay((MAX_FADE - fade) >> 4);
+    }
+  }
+
+  // main loop
+  while (1) {
+    loop();
+  }
+
+  return 0;
+}
